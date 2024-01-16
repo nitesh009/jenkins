92376bda28322db83462d74f83cf3ec34c0626c9
Aditi.SinghJadoun <aditi.singhjadoun@cognizant.com>
2024-01-16 10:05:11 +0530

    added an endpoint

--- src/main/java/com/nkdebug/jenkins/JenkinsApplication.java
+++ src/main/java/com/nkdebug/jenkins/JenkinsApplication.java
@@ -4,11 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
 
 	static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
@@ -18,6 +22,10 @@ public class JenkinsApplication {
 		logger.info("Application Started");
 	}
 
+	@GetMapping("/print")
+	public String myMethod() {
+		return "Welcome";
+	}
 	public static void main(String[] args) {
 		logger.info("Application executed");
 		SpringApplication.run(JenkinsApplication.class, args);
