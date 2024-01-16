ed4e910cce9deb624a087823de2985de106fd6f9
sai.bedamatta <sai.bedamatta@cognizant.com>
2024-01-16 10:35:11 +0530

    Rest Endpoint hello is added

--- src/main/java/com/nkdebug/jenkins/JenkinsApplication.java
+++ src/main/java/com/nkdebug/jenkins/JenkinsApplication.java
@@ -4,23 +4,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+
+/**
+ * @author sai
+ */
+
 
 @SpringBootApplication
 public class JenkinsApplication {
 
-	static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+    static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
 
-	@PostConstruct
-	public void test() {
-		logger.info("Application Started");
-	}
+    @PostConstruct
+    public void test() {
+        logger.info("Application Started");
+    }
 
-	public static void main(String[] args) {
-		logger.info("Application executed");
-		SpringApplication.run(JenkinsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        logger.info("Application executed");
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+    
+    @RestController
+    @RequestMapping("/api")
+    class MyRestController {
 
+        @GetMapping("/hello")
+        public String helloEndpoint() {
+            return "Hello, world!";
+        }
+    }
 }
